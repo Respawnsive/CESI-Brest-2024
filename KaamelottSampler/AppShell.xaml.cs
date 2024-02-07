7061623a3c254d3af78e088cd0b2be21c849d623
Jérôme Liger <jerome.liger@respawnsive.com>
2024-02-07 15:58:42 +0100

    Initial commit

---
+++ KaamelottSampler/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KaamelottSampler
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KaamelottSampler/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KaamelottSampler
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
