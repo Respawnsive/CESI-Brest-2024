38b8a6840f1946554380a2a73756291fd6c53509
Jérôme Liger <jerome.liger@respawnsive.com>
2024-03-08 12:15:32 +0100

    Ajout bouton synchronisation

--- KaamelottSampler/App.xaml.cs
+++ KaamelottSampler/App.xaml.cs
@@ -13,7 +13,7 @@ namespace KaamelottSampler
         {
             InitializeComponent();
 
-            AppCenter.Start("e1f5a50e-6236-4416-8acd-ebe290f4948b", typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("android =ba94abba-5b2f-433f-8e48-6e8e2f4cdd9e;uwp=e1f5a50e-6236-4416-8acd-ebe290f4948b", typeof(Analytics), typeof(Crashes));
 
             Services = ConfigureServices();
 
